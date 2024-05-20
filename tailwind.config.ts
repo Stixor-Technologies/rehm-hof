1ce5e7d6b46f94278772e90d66462585abc28c11
imrankhan-stixor <154209226+imrankhan-stixor@users.noreply.github.com>
2024-05-20 16:36:44 +0500

    added main colors in the tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,18 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: "#2621C5",
+        secondary: "#322E2D",
+        secondaryLight: "#707070",
+        secondaryDark: "#34302D",
+        white: "#FFFFFF",
+        black: "#000000",
+        bone: "#DCD6C8",
+        boneFade: "#DCD6C800",
+        navBackground: "#00000029",
+        slate: "#F0F1F1",
+      },
     },
   },
   plugins: [],
