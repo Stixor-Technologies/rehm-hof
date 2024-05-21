09fa7639165c7175be2df13c62900f7ce87609de
imrankhan-stixor <154209226+imrankhan-stixor@users.noreply.github.com>
2024-05-21 18:45:49 +0500

    added a template for the reusable pages, made changes and completed the datenschutz page

---
+++ components/impressum-datenschutz-template/index.tsx
@@ -0,0 +1,34 @@
+import React, { ReactNode } from "react";
+import Image from "next/image";
+
+const ImpressumDatenschutzTemplate = ({
+  title,
+  children,
+}: {
+  title: string;
+  children: ReactNode;
+}) => {
+  return (
+    <div className="bg-white text-secondary">
+      <div className="bg-impressum-datenschutz-header relative min-h-[25rem] w-screen bg-cover bg-center bg-no-repeat">
+        <Image
+          src={"/assets/images/key.svg"}
+          alt="Key image"
+          width={150}
+          height={40}
+          className="absolute right-0 top-8"
+        />
+      </div>
+
+      <div className="container py-[6.25rem]">
+        <h1 className="pb-5 text-2xl uppercase text-primary md:pb-[6.25rem] lg:text-4xl xl:text-6xl">
+          {title}
+        </h1>
+
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default ImpressumDatenschutzTemplate;
