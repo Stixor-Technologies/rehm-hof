24037e61293dded99021d91a7a87b7f0e4061f79
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-05-23 09:31:41 +0500

    linting errors

--- app/(landing-page)/page.tsx
+++ app/(landing-page)/page.tsx
@@ -141,7 +141,7 @@ const LandingPage = () => {
           </div>
         </div>
         <div
-          className="bg-hero-gradient absolute inset-0 flex items-center "
+          className="absolute inset-0 flex items-center bg-hero-gradient "
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
         >
