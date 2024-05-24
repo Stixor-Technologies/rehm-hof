b1988d86edd4ac1e50e6c4e4ffa4fe4bf0ede097
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-05-24 10:42:48 +0500

    added project heading

--- app/projekt/page.tsx
+++ app/projekt/page.tsx
@@ -1,7 +1,36 @@
 import React from "react";
 
 const Projekt = () => {
-  return <div>Projekt</div>;
+  return (
+    <div>
+      <section>
+        <div className="container">
+          <section className="w-full  max-w-[47.5rem] lg:my-[6.25rem]">
+            <h2 className=" text-[clamp(2.063rem,4vw,4.063rem)] text-primary 2xl:leading-[4.688rem]">
+              PROJEKT BESCHREIBUNG
+            </h2>
+            <p className="tracking-[0.025rem] text-secondary lg:mt-[3.84rem] xl:text-xl xl:leading-[2rem]">
+              Der Innenhof des „Rehm-Hof“ bietet eine harmonische Ruheoase im
+              lebendigen Winterhude. Eine stilvolle Gartenanlage lädt zum
+              Entspannen und Verweilen ein, während man sich dennoch mitten im
+              Geschehen befindet. Dieses Neubauprojekt schafft eine einzigartige
+              Balance zwischen privater Rückzugsmöglichkeit und dem lebhaften
+              Treiben Winterhudes und ist somit die perfekte Adresse für
+              anspruchsvolles, modernes Wohnen in Hamburg.
+            </p>
+
+            <p className="mt-8 tracking-[0.025rem] text-secondary xl:text-xl xl:leading-[2rem]">
+              Ein weiteres Highlight ist die Tiefgarage. Die zukünftigen
+              Bewohner können die Vorzüge des innerstädtischen Lebens in vollen
+              Zügen genießen, ohne auf den Luxus eines eigenen Stellplatzes
+              verzichten zu müssen. Hier entsteht nicht nur ein Zuhause, sondern
+              ein Lifestyle, der Exklusivität und urbanen Chic perfekt vereint.
+            </p>
+          </section>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Projekt;
