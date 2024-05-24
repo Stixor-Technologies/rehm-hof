a09a1668c366818e6c964292b2717c10e936675b
imrankhan-stixor <154209226+imrankhan-stixor@users.noreply.github.com>
2024-05-24 11:50:10 +0500

    fixed navbar changes

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <div className="flex h-[5rem] w-full items-center justify-between gap-4 bg-white px-5 sm:h-[7.688rem] md:px-8 lg:px-[3.75rem]">
-      <div className="flex h-9 w-9 cursor-pointer flex-col gap-2.5 sm:w-14  sm:gap-3">
+      <div className="flex h-[2.125rem] w-9 cursor-pointer flex-col gap-2.5 sm:w-[3.125rem]  sm:gap-3">
         <div className="border border-black sm:border-2" />
         <div className="border border-black sm:border-2" />
         <div className="border border-black sm:border-2" />
