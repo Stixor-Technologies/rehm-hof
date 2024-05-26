1f02cbe3225c070cc7b794d59e9b64ee3e789b14
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-05-27 01:04:08 +0500

    fixed collage section lage page

--- app/lage/page.tsx
+++ app/lage/page.tsx
@@ -147,15 +147,15 @@ const Lage = () => {
     <section>
       {/* section# 1 */}
 
-      <section className="mx-auto mt-10 flex max-w-[1920px] flex-col gap-10 bg-slate px-4 pb-[4.535rem] pt-12 sm:px-8 lg:mt-[5.625rem] lg:flex-row lg:pr-0 lg:pt-[5.84rem] 4xl:pl-[11.25rem]">
-        <div className=" flex-1   xl:ml-[10.313rem] ">
-          <p className=" w-full text-[clamp(2rem,5.5vw,6.563rem)] uppercase leading-tight text-secondary 2xl:leading-[6.75rem]">
-            Leben <br /> Zwischen <br /> stadtpark <br />
-            <span className=" font-semibold text-primary">& </span>
+      <section className="mx-auto mt-10 flex max-w-[1920px] flex-col gap-10 bg-slate px-4 py-12 sm:px-8 lg:mt-[6.25rem] lg:flex-row lg:py-[5.188rem] lg:pr-0 4xl:pl-[11.25rem]">
+        <div className=" flex-1  xl:ml-[10.313rem] ">
+          <p className=" w-full text-[clamp(2rem,5.5vw,6.563rem)] uppercase leading-tight text-secondary lg:max-w-[600px] 2xl:leading-[6.75rem] 4xl:h-[463px]">
+            Leben Zwischen stadtpark
+            <span className=" font-bold text-primary"> & </span>
             alster
           </p>
 
-          <p className="mt-4 text-[clamp(1.2rem,2vw,2.188rem)]  tracking-[0.044rem] text-secondary lg:mt-[3.563rem] lg:max-w-[775px] 2xl:leading-[3.125rem]">
+          <p className="mt-4 text-[clamp(1.2rem,2vw,2.188rem)]  tracking-[0.044rem] text-secondary lg:mt-[1.875rem] lg:max-w-[48.438rem] 2xl:leading-[3.125rem]">
             NATÜRLICH URBAN LEBEN: WASSER, PARK & DAS PULSIERENDE LEBEN VOR DER
             TÜR - DEIN ZUHAUSE IM HERZEN DES GESCHEHENS
           </p>
@@ -165,26 +165,19 @@ const Lage = () => {
           height={634}
           src={Leben}
           alt=""
-          className="w-full lg:w-[50%] lg:max-w-[672px] 2xl:w-full"
+          className="max-h-[634px] w-full lg:w-[50%] lg:max-w-[672px] 2xl:w-full"
         />
       </section>
 
       <section className="container">
         {/* section# 2 */}
-        <div className="my-[6.375rem] flex flex-col items-start gap-10 lg:flex-row">
-          <Image
-            src={HamburgWinterHude}
-            width={760}
-            height={618}
-            alt="Hamburg-WinterHude"
-            className="w-full lg:max-w-[760px] lg:flex-1 2xl:flex-none"
-          />
-          <div className=" flex-1">
-            <h2 className=" text-[clamp(2.063rem,4vw,4.063rem)] leading-none text-primary 2xl:leading-[4.688rem]">
+        <div className="mb-[6.375rem] mt-[6.438rem] flex flex-col items-start gap-10 lg:flex-row">
+          <div className="flex-1 lg:order-last lg:w-1/2 lg:max-w-[760px]">
+            <h2 className=" text-[clamp(2.063rem,4vw,4.063rem)] leading-none text-primary 2xl:leading-[4.688rem] 3xl:w-[834px]">
               HAMBURG-WINTERHUDE
             </h2>
 
-            <div className=" hamburg-winter-text mt-5 space-y-4 xl:mt-[3.75rem] 2xl:space-y-10">
+            <div className=" hamburg-winter-text mt-5 space-y-4 xl:mt-[3.75rem] 2xl:space-y-7">
               <p>
                 Willkommen in einem gewachsenem Stadtteil voller Charme und
                 urbaner Raffinesse. Die Umgebung vereint auf einzigartige Weise
@@ -217,6 +210,52 @@ const Lage = () => {
               </p>
             </div>
           </div>
+
+          <Image
+            src={HamburgWinterHude}
+            width={760}
+            height={618}
+            alt="Hamburg-WinterHude"
+            className="w-full lg:max-w-[760px] lg:flex-1 2xl:flex-none"
+          />
+          {/* <div className="flex-1 lg:w-1/2 lg:max-w-[760px]">
+            <h2 className=" text-[clamp(2.063rem,4vw,4.063rem)] leading-none text-primary 2xl:leading-[4.688rem] 3xl:w-[834px]">
+              HAMBURG-WINTERHUDE
+            </h2>
+
+            <div className=" hamburg-winter-text mt-5 space-y-4 xl:mt-[3.75rem] 2xl:space-y-7">
+              <p>
+                Willkommen in einem gewachsenem Stadtteil voller Charme und
+                urbaner Raffinesse. Die Umgebung vereint auf einzigartige Weise
+                das geschäftige Treiben der Stadt mit einer Fülle von
+                gastronomischen Erlebnissen sowie Sport- und
+                Freizeitmöglichkeiten.
+              </p>
+
+              <p>
+                Winterhudes Herzstück ist dort zu finden, wo belebte Straßen von
+                einer Vielzahl von Geschäften, Boutiquen, Restaurants und Cafés
+                gesäumt werden. Die kulinarische Vielfalt spiegelt die
+                Atmosphäre des Stadtteils wider und lässt Foodie-Herzen
+                höherschlagen.
+              </p>
+
+              <p>
+                Das lebhafte Treiben zeigt sich nicht nur in den Straßen,
+                sondern auch in einer breiten Palette von Sport- und
+                Freizeitmöglichkeiten. Der Stadtpark und das Alsterufer laden zu
+                entspannten Spaziergängen oder ausgedehnten Laufrunden ein,
+                während moderne Fitnessstudios und exklusive Spa- und
+                Wellnessclubs die sportlichen Ambitionen und das Wellbeing der
+                Bewohner unterstützen. Wassersportliebhaber können zudem die
+                nahegelegene Alster für ihre Aktivitäten nutzen, während der
+                Stadtpark Raum für Konzerte, Sportveranstaltungen und erholsame
+                Stunden im Grünen bietet. Des Weiteren zeichnen sich die
+                Bildungseinrichtungen in Winterhude sich durch höchste Qualität
+                aus.
+              </p>
+            </div>
+          </div> */}
         </div>
 
         {/* section# 3 */}
@@ -224,13 +263,15 @@ const Lage = () => {
           <div className="flex-1">
             <Image src={Alster} width={760} height={487} alt="" />
           </div>
-          <div className="grid w-full max-w-[47.5rem] flex-1 grid-cols-[26.5%_21%_52.6%] grid-rows-[28.4%_49.3%_19%]">
+
+          {/* grid-cols-[26.5%_21%_52.6%] grid-rows-[28.4%_49.3%_19%] */}
+          <div className="grid w-full max-w-[47.5rem] flex-1 grid-cols-[26.318%_21.055%_52.633%] grid-rows-[27.19%_47.2%_18.18%]">
             <div className="col-span-2  col-start-2 row-span-2 row-start-1 overflow-hidden">
               <Image
                 ref={image1Ref}
                 src={Markt}
                 alt=""
-                className="relative z-10 h-full"
+                className="relative z-10 h-full object-cover"
                 onMouseEnter={handleMouseEnter}
                 onMouseLeave={handleMouseLeave}
               />
